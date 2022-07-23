44be279058ed7faa9b45661c0336a900727a5b4a
Toms <11509310+tomzs@users.noreply.github.com>
2022-07-23 12:44:43 +0300

    added more game state logic

--- src/shape.rs
+++ src/shape.rs
@@ -1,7 +1,13 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, ops::Add};
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 pub struct Pos(pub i32, pub i32);
+impl Add for Pos {
+    type Output = Pos;
+    fn add(self, rhs: Pos) -> Pos {
+        Pos(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
 #[derive(Debug, Clone)]
 pub struct Shape {
     positions: HashSet<Pos>,
@@ -44,6 +50,32 @@ impl Shape {
             5 => Self::new_s(),
             6 => Self::new_z(),
             _ => unreachable!(),
+        }
+    }
+    pub fn positions(&self) -> impl Iterator<Item = Pos> + '_ {
+        self.positions.iter().copied()
+    }
+    pub fn collides_with(&self, other: &Shape) -> bool {
+        self.positions.intersection(&other.positions).count() > 0
+    }
+    pub fn rotate(&self) -> Self {
+        let Pos(a, b) = self.anchor;
+        Self {
+            positions: self
+                .positions()
+                .map(|Pos(x, y)| Pos(-y + b + a, x - a + b))
+                .collect(),
+            anchor: self.anchor,
+        }
+    }
+}
+
+impl Add<Pos> for &Shape {
+    type Output = Shape;
+    fn add(self, rhs: Pos) -> Self::Output {
+        Shape {
+            positions: self.positions.iter().map(|&pos| pos + rhs).collect(),
+            anchor: self.anchor + rhs,
         }
     }
 }
--- src/tetris.rs
+++ src/tetris.rs
@@ -1,20 +1,65 @@
-use crate::shape::Shape;
+use std::mem;
+
+use crate::shape::{Pos, Shape};
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Direction {
+    Left,
+    Right,
+}
 #[derive(Debug)]
 pub struct Tetris {
-    width: usize,
-    height: usize,
+    width: i32,
+    height: i32,
     current_shape: Shape,
     placed_shape: Vec<Shape>,
 }
 
 impl Tetris {
-    pub fn new(width: usize, height: usize) -> Self {
+    pub fn new(width: u32, height: u32) -> Self {
         Self {
-            width,
-            height,
-            current_shape: Shape::new_random(),
+            width: width as i32,
+            height: height as i32,
+            current_shape: &Shape::new_random() + Pos((width as i32) / 2, 0),
             placed_shape: vec![],
         }
+    }
+    pub fn is_out_of_bounds(&self, shape: &Shape) -> bool {
+        shape
+            .positions()
+            .all(|pos| 0 <= pos.0 && pos.0 < self.width && 0 <= pos.1 && pos.1 < self.height)
+    }
+    pub fn is_colliding(&self, shape: &Shape) -> bool {
+        self.placed_shape
+            .iter()
+            .any(|placed_shape| shape.collides_with(placed_shape))
+    }
+
+    pub fn tick(&mut self) {
+        let translated_curr_shape = &self.current_shape + Pos(0, 1);
+        if self.is_out_of_bounds(&translated_curr_shape)
+            || self.is_colliding(&translated_curr_shape)
+        {
+            let newly_placed_shape = mem::replace(
+                &mut self.current_shape,
+                &Shape::new_random() + Pos((self.width) / 2, 0),
+            );
+            self.placed_shape.push(newly_placed_shape);
+        } else {
+            self.current_shape = translated_curr_shape;
+        }
+    }
+    pub fn shift_current_shape(&mut self, direction: Direction) {
+        let translated_current_shape = &self.current_shape
+            + match direction {
+                Direction::Left => Pos(-1, 0),
+                Direction::Right => Pos(1, 0),
+            };
+            if !self.is_out_of_bounds(&translated_current_shape)
+                && !self.is_colliding(&translated_current_shape)
+            {
+                self.current_shape = translated_current_shape;
+            }
+            pub fn rotation
     }
 }
 #[cfg(test)]
@@ -22,6 +67,7 @@ mod tests {
     use super::Tetris;
     #[test]
     fn test() {
-        println!("{:#?}", Tetris::new(10, 10));
+        let mut tetris = Tetris::new(10, 30);
+        println!("{:#?}", Tetris::new(10, 30));
     }
 }
